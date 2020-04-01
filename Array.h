957d8aeb5cedd4bdd8b1135b05390ac228dcdaf5
Zanic23 <57819528+Zanic23@users.noreply.github.com>
2020-04-01 14:58:21 -0600

    Update Array.h

--- Array.h
+++ Array.h
@@ -1,38 +1,12 @@
-#pragma once
-#include "value.h"
-#include <vector>
-#include "ValueType.h"
-#include "ValueRegistration.h"
-#include <type_traits>
-#include "Parser.h"
-
-namespace jbon {
-	template <typename T>
-	class Array :
-		public TemplateValueTypeContract<Array<T>>
-	{
-	private:
-	//implemented functions
-		static Array<T> * create(std::string str)
-		{
-			return new Array<T>;
-		}
-		static bool isSameType(std::string str)
-		{
-		...
-		}
-		static std::pair<char, char> getSurroundingDelimeters()
-		{
-		...
-		}
-
-	public:
-		Array() {}
-		~Array() {}
-
-		std::string serialize()
-		{
-			...
-		}
-	};
-}
+//example
+template <typename T>
+class Array :
+	public TemplateValueTypeContract<Array<T>>
+{
+private:
+//dummy returns
+	static Array<T> * create(std::string str) { return new Array<T>; }
+	static bool isSameType(std::string str) { return true; }
+	static std::pair<char, char> getSurroundingDelimeters() { return {',',','}; }
+	std::string serialize() { return ""; }
+};
