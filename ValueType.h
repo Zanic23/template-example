ab7f50d78956a07e65a2d5cfd0defd96085af985
Zanic23 <57819528+Zanic23@users.noreply.github.com>
2020-04-01 14:52:23 -0600

    Update ValueType.h

--- ValueType.h
+++ ValueType.h
@@ -26,31 +26,6 @@ namespace jbon {
 		virtual ~ValueType() {}
 	};
 
-	template <typename T>
-	class ValueTypeContract :
-		public ValueType
-	{
-	private:
-		//register the class
-		static inline ValueRegistration<T> registered;
-		virtual std::string serialize() = 0;
-		//these wont ever actually be used, but they require the child to implement the static methods
-		bool virtualIsSameType(std::string str) {
-			return T::isSameType(str);
-		}
-		//should return malloced pointer of child
-		ValueType * virtualCreate(std::string str) {
-			return T::create(str);
-		}
-		std::pair<char, char> virtualGetSurroundingDelimeters() {
-			return T::getSurroundingDelimeters();
-		}
-	public:
-		ValueTypeContract() {}
-		virtual ~ValueTypeContract() {}
-		
-		//user still needs to implement serialize()
-	};
 	//specialized template contract
 	template <template<typename U> class T>
 	class TemplateValueTypeContract :
@@ -78,17 +53,5 @@ namespace jbon {
 
 		//user still needs to implement serialize()
 	};
-	template<typename D, typename B>
-	class isDerived {
-		class No {};
-		//just to make sure yes is different in size than no
-		class Yes { No no[3]; };
-
-		static Yes Test(B*);
-		static No Test(...);
-	public:
-		enum {is = sizeof(Test(static_cast<D*>(0))) == sizeof(Yes) };
-	};
-
 }
 
