b5f3571bf6f26fb569b0eac4be6f4aa844c2e648
Zanic23 <57819528+Zanic23@users.noreply.github.com>
2020-04-01 15:00:41 -0600

    Update Array.h

--- Array.h
+++ Array.h
@@ -10,3 +10,34 @@ private:
 	static std::pair<char, char> getSurroundingDelimeters() { return {',',','}; }
 	std::string serialize() { return ""; }
 };
+
+
+
+
+
+//USAGE
+
+class ValueType;
+//struct that contains all the information about a current registered classes
+struct Registrants {
+	typedef std::vector<std::pair<std::function<bool(std::string)>,
+		std::function<ValueType*(std::string)>>> vec_t;
+	typedef std::vector<std::pair<char, char>> delimeters_t;
+private:
+	//never deleted because itll get chopped randomly with smart ptrs
+	static inline vec_t * registrations;
+	static inline delimeters_t * delimeters;
+};
+
+template <template <typename U> class T>
+class ValueRegistration :
+	public Registrants
+{
+public:
+	ValueRegistration() {
+
+		auto pair = std::make_pair(&T<U>::isSameType, createFunction(T<U>::create));
+		getRegistrations()->push_back(pair);
+		getDelimeters()->push_back(T<U>::getSurroundingDelimeters());
+	}
+};
